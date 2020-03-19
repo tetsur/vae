c56c153881779e46c769195e5d1401d79ca7574f
tetsur <tetsur00723@gmail.com>
2020-03-19 06:47:19 +0000

    for monochrome

--- text_to_image/word2vec.py
+++ text_to_image/word2vec.py
@@ -2,6 +2,6 @@ from gensim.models import word2vec
 import os
 
 sentence = word2vec.Text8Corpus('birds.txt')
-model = word2vec.Word2Vec(sentence,  sg=1, size=30, min_count=1, window=5, hs=0, negative=15, iter=15)
+model = word2vec.Word2Vec(sentence,  sg=1, size=10, min_count=1, window=5, hs=0, negative=15, iter=15)
 
 model.save('birds_txt.model')
